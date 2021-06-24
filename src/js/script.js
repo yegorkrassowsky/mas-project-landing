e61621c77a51435b2dd78ecc79793a49ebe06eb8
Krassowsky <krassowsky@MacBook-Pro-Krassowsky.local>
2021-06-24 17:16:22 +0800

    Feedback Section added

--- src/js/script.js
+++ src/js/script.js
@@ -24,6 +24,46 @@ $(function() {
     }
   }).mount();
 
+  const splideFeedback = new Splide( '#splide-feedback', {
+    type: 'loop',
+    arrows: false,
+    perPage: 3,
+    perMove: 1,
+    pagination: false,
+    breakpoints: {
+      1000: {
+        focus: 'center',
+      },
+      690: {
+        perPage: 1,
+      },
+    }
+  });
+
+  function splideFeedbackUpdate(){
+    $('.splide-count').text(`${splideFeedback.index+1}/${splideFeedback.length}`);
+    const activeSlide = splideFeedback.Components.Elements.slides[splideFeedback.index];
+    const newText = $('.feedback-card', activeSlide).data('text');
+    $('.splide-text').text(newText);
+  }
+
+  splideFeedback.on('mounted', splideFeedbackUpdate);
+  splideFeedback.on('active', splideFeedbackUpdate);
+
+  $('#splide-feedback').find('.card-number').each(function(index, el){
+    $(el).text((index+1).toString().padStart(2, '0'));
+  });
+
+  splideFeedback.mount();
+
+  $('#splide-feedback-arrow-prev').on('click', function(){
+    splideFeedback.go('-');
+  });
+
+  $('#splide-feedback-arrow-next').on('click', function(){
+    splideFeedback.go('+');
+  });
+
   $('.nav-toggle-open').on('click', function(){
     $(this).parents('.nav-menu').addClass('open')
   });
